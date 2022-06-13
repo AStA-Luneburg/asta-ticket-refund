73f11bdb6f2f68125b4ca347d2d570221f82bc00
Lukas Mateffy <github@mateffy.me>
2022-06-13 17:23:41 +0200

    Reformatted Confirmation Mail

--- resources/views/mails/submit-confirmation.blade.php
+++ resources/views/mails/submit-confirmation.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <h1>{{ __('app.mails.submit-confirmation.title') }}</h1>
     <p>{{ __('app.mails.hello') }}</p>
-    <p>{!! __('app.mails.submit-confirmation.message', ['name' => config('app.asta-name')]) !!}</p>
     <p>
+        {!! __('app.mails.submit-confirmation.message', ['name' => config('app.asta-name')]) !!}
         {!! __('app.mails.submit-confirmation.support', ['email' => config('app.support-mail')]) !!}
     </p>
     <p>{{ __('app.mails.your-team') }}</p>
