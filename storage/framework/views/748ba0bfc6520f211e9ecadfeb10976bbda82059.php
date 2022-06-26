553cc72697eb5908cac8e9f4b1e43b431b00dfc4
Lukas Mateffy <github@mateffy.me>
2022-06-26 22:26:27 +0200

    Cached artisan

---
+++ storage/framework/views/748ba0bfc6520f211e9ecadfeb10976bbda82059.php
@@ -0,0 +1,22 @@
+<?php foreach($attributes->onlyProps(['active']) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+} ?>
+<?php $attributes = $attributes->exceptProps(['active']); ?>
+<?php foreach (array_filter((['active']), 'is_string', ARRAY_FILTER_USE_KEY) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+} ?>
+<?php $__defined_vars = get_defined_vars(); ?>
+<?php foreach ($attributes as $__key => $__value) {
+    if (array_key_exists($__key, $__defined_vars)) unset($$__key);
+} ?>
+<?php unset($__defined_vars); ?>
+
+<?php
+$classes = $active ?? false ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-slate-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out' : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-slate-500 hover:text-slate-700 hover:border-slate-300 focus:outline-none focus:text-slate-700 focus:border-slate-300 transition duration-150 ease-in-out';
+?>
+
+<a <?php echo e($attributes->merge(['class' => $classes])); ?>>
+    <?php echo e($slot); ?>
+
+</a>
+<?php /**PATH /Users/mat/Projects/AStA/asta-ticket-refund/resources/views/components/nav-link.blade.php ENDPATH**/ ?>
