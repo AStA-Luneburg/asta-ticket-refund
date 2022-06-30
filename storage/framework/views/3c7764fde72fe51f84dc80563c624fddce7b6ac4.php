38f46e233b8a096f4a47cd23cf8f5cc7c22b9e24
Lukas Mateffy <github@mateffy.me>
2022-06-30 22:56:08 +0200

    Updated welcome page text

--- storage/framework/views/3c7764fde72fe51f84dc80563c624fddce7b6ac4.php
+++ storage/framework/views/3c7764fde72fe51f84dc80563c624fddce7b6ac4.php
@@ -34,14 +34,76 @@
 <?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
 <?php endif; ?>
 <?php $component->withAttributes(['class' => 'text-xl  text-asta-blue-900 mb-10 leading-relaxed']); ?>
-        <p class="mb-5">
-            <?php echo __('app.welcome.welcome-1'); ?>
-
-        </p>
-        <p class="mb-20">
-            <?php echo __('app.welcome.welcome-2'); ?>
-
-        </p>
+        <?php if(App::getLocale() === 'de'): ?>
+            <p class="mb-5">
+                <strong>Liebe Studierende</strong>,
+            </p>
+            <p class="mb-5">
+                Für euer Semesterticket, das ihr für den Busverkehr im Landkreis Lüneburg und die Nahverkehrszüge in Niedersachsen und Bremen nutzen könnt, habt ihr im Sommersemester 2022 157,67 € bezahlt.
+            </p>
+            <p class="mb-5">
+                Mit der Einführung des sogenannten 9-Euro-Tickets wurde euer Semesterticket für die Monate Juni, Juli und August obsolet.
+                Für diese Monate zahlen wir den Bus- und Bahnunternehmen folglich nur 9 Euro pro Student*in und Monat.
+            </p>
+            <p class="mb-5">
+                Doch was heißt das konkret für euch?
+            </p>
+            <p class="mb-5">
+                <strong>Ihr bekommt daher 51,84 € eures Semesterbeitrages für das Sommersemester 2022 zurück!</strong>
+            </p>
+            <p class="mb-5">
+                Da wir eure Kontoverbindungen nicht vorliegen haben, können wir diesen Beitrag nicht automatisch an alle Studierende überweisen.
+            </p>
+            <p class="mb-5">
+                <strong>Daher muss jede*r Student*in die Erstattung der 51,84 € über diese Website beantragen.</strong>
+            </p>
+            <p class="mb-5">
+                Antragsberechtigt sind alle Studierenden, die im Sommersemester 2022 ihren Semesterbeitrag gezahlt haben.<br>
+                <i>Ausgenommen: Studierende der Professional School und Studierende, die eine Rückerstattung auf anderem Wege erhalten haben.</i>
+            </p>
+            <p class="mb-5">
+                Die Möglichkeit der Rückerstattung steht allen antragsberechtigten Studierenden bis zum 31. Dezember 2025 zur Verfügung. Wir bitten euch, die Antragstellung möglichst schnell vorzunehmen.
+            </p>
+            <p class="mb-20">
+                Mit besten Grüßen aus dem AStA,<br>
+                <strong>Deine AStA-Sprecher*innen</strong>
+            </p>
+        <?php else: ?>
+            <p class="mb-5">
+                <strong>Dear students</strong>,
+            </p>
+            <p class="mb-5">
+                For your semester ticket, which you can use for bus transport in the district of Lüneburg and local trains in Lower Saxony and Bremen, you paid 157,67 € in the summer semester 2022.
+            </p>
+            <p class="mb-5">
+                With the introduction of the so-called 9-Euro-Ticket, your semester ticket became obsolete for the months of June, July and August.
+                For these months, we will therefore only pay the bus and train companies €9 per student per month.
+            </p>
+            <p class="mb-5">
+                But what does that mean for you?
+            </p>
+            <p class="mb-5">
+                <strong>You will get back 51,84 € of your semester fee for the summer semester 2022!</strong>
+            </p>
+            <p class="mb-5">
+                Since we do not have your bank account details, we cannot automatically transfer this fee to all students.
+            </p>
+            <p class="mb-5">
+                <strong>Therefore, each student has to apply for the refund of the 51,84 € via this website.</strong>
+            </p>
+            <p class="mb-5">
+                All students who have paid their semester fee in the summer semester 2022 are eligible to apply.<br>
+                <i>Excluded: Professional School students and students who have received a refund through other means.</i>
+            </p>
+            <p class="mb-5">
+                The refund option is available to all eligible students until December 31, 2025. We ask that you apply as soon as possible.
+            </p>
+            <p class="mb-20">
+                With best regards from the AStA,<br>
+                <strong>Your AStA spokespersons</strong>
+            </p>
+        <?php endif; ?>
+        
         <h2 class=" text-3xl text-center font-semibold mb-20">
             <?php echo e(__('app.welcome.how-does-it-work')); ?>
 
